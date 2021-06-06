df6c801641f3dd9c2a5fbe1d7e64d8a385ae1908
Emry Brisky <eabrisky@gmail.com>
2021-06-06 15:40:24 -0500

    updated database.sql

--- database.sql
+++ database.sql
@@ -10,6 +10,18 @@ CREATE TABLE gallery (
 );
 
 INSERT INTO gallery (description, likes, path)
-VALUES ('Photo of a goat taken at Glacier National Park.', 0,'images/goat_small.jpg');
+VALUES ('This statue is a depiction of the four cardinal directions, personified.', 0, 'images/cardinal.jpg');
+
+INSERT INTO gallery (description, likes, path)
+VALUES ('My friend and I messing around with a 35mm camera.', 0, 'images/friend.jpg');
+
+INSERT INTO "gallery" (description, likes, path)
+VALUES ('The Ice Age National Trail runs through the Driftless region of Wisconsin, and views of the many bluffs can be seen throughout.', 0, 'images/bluffs.jpg');
+
+INSERT INTO gallery (description, likes, path)
+VALUES ('Dad has been golfing since he was able to stand. Occasionally, my sister and I get to go with!', 0, 'images/family.jpg');
+
+INSERT INTO gallery (description, likes, path)
+VALUES ('This is an old BTS photo from a music video a couple years back.', 0, 'images/flowers.jpg');
 
 SELECT * FROM "gallery";
